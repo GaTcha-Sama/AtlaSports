dac7f991b3835940bb8f05e056dcdfe3331e61c8
Jeremy Louet <jeremy.louet29@hotmail.fr>
2024-05-18 17:53:55 +0200

    Ajout prototype header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import '../styles/Header.scss'
 
 function Header() {
   return (
     <div className='header'>
+      <div className='header__title'>
       ATLAS DES SPORTS US
+      </div>      
     </div>
   )
 }
