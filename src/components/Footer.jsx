b855f3326115f6823daba00aca54a76efdfb1f62
Jeremy Louet <jeremy.louet29@hotmail.fr>
2024-05-18 15:19:56 +0200

    Ajout des RS dans le footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import '../styles/Footer.scss'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faInstagram, faGithub, faXTwitter} from '@fortawesome/free-brands-svg-icons'
+import {faInstagram, faGithub, faXTwitter, faFacebook, faLinkedin} from '@fortawesome/free-brands-svg-icons'
 
 function Footer() {
   return (
     <div className='footer'>
       <div className='footer__medias'>
-        <FontAwesomeIcon icon={faGithub} />
-        <FontAwesomeIcon icon={faInstagram} />
-        <FontAwesomeIcon icon={faXTwitter} />
+        <FontAwesomeIcon icon={faGithub} className='footer__medias__icon' />
+        <FontAwesomeIcon icon={faLinkedin} className='footer__medias__icon' />
+        <FontAwesomeIcon icon={faInstagram} className='footer__medias__icon' />
+        <FontAwesomeIcon icon={faXTwitter} className='footer__medias__icon' />
+        <FontAwesomeIcon icon={faFacebook} className='footer__medias__icon' />
       </div>
       Made by Jeremy Louet in 2024. © All rights reserved.
     </div>
