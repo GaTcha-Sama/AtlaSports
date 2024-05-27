a9953ab9210d46a53e0c7ed01b4f59af935c4771
Jeremy Louet <jeremy.louet29@hotmail.fr>
2024-05-27 13:42:44 +0200

    Ajout layer USA

--- src/components/Home/Gallery-USA.jsx
+++ src/components/Home/Gallery-USA.jsx
@@ -1,33 +1,35 @@
-import React from 'react'
-import '../../styles/Gallery.scss'
-import {useNavigate} from 'react-router-dom'
-import DataUSA from '../../data/data-flag-states-usa.json'
+import React from 'react';
+import '../../styles/Gallery.scss';
+import { useNavigate } from 'react-router-dom';
+import DataUSA from '../../data/data-flag-states-usa.json';
 
 function GalleryUSA() {
-const navigate = useNavigate()
+  const navigate = useNavigate();
 
-const handleItemClick = (item) => {
-  navigate(`/state/${item.id}`)
+  const handleItemClick = (item) => {
+    navigate(`/state/${item.id}`);
+  };
+
+  return (
+    <>
+      <section>
+        <h1 className='country'>🇺🇸​​ UNITED STATES OF AMERICA 🇺🇸​​</h1>
+      </section>
+      <section className="gallery">
+        {DataUSA.map((item) => (
+          <div key={item.id} className="gallery__item">
+            <button type="button" className='gallery__card' onClick={() => handleItemClick(item)}>
+              <img src={item.drapeau} alt={`${item.nom} flag`} className="gallery__img" />
+              <h2 className="gallery__title">{item.nom}</h2>
+              <div className="gallery__overlay">
+                <img src={item.carte} alt={`${item.nom} map`} className="gallery__map" />
+              </div>
+            </button>
+          </div>
+        ))}
+      </section>
+    </>
+  );
 }
 
-return (
-  <>
-  <section>
-    <h1 className='country'>🇺🇸​​ UNITED STATES OF AMERICA 🇺🇸​​</h1>
-  </section>
-  <section className="gallery">
-    {DataUSA.map((item) => (
-      <div key={item.id}>
-        <button type="button" className='gallery__card' onClick={() => handleItemClick(item)}>
-          <img src={item.drapeau} alt="" className="gallery__img" />
-          <h2 className="gallery__title">{item.nom}</h2>
-        </button>
-      </div>
-    ))}
-  </section>
-  </>
-)
-
-}
-
-export default GalleryUSA
+export default GalleryUSA;
