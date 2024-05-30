2fa5d7e6b99bfd9c9703432b2eb04cfd88ad0be8
Jeremy Louet <jeremy.louet29@hotmail.fr>
2024-05-30 16:43:54 +0200

    Début structuration page State

--- src/components/State/Logos.jsx
+++ src/components/State/Logos.jsx
@@ -1,11 +1,78 @@
 import React from 'react'
 import '../../styles/Logos.scss'
+import LogoNBA from '../../assets/Logos/ARIZONA/Phoenix Suns.png'
+import LogoNFL from '../../assets/Logos//ARIZONA/Arizona Cardinals.png'
+import LogoNHL from '../../assets/Logos//ARIZONA/Arizona Coyotes.png'
+import LogoMLB from '../../assets/Logos//ARIZONA/Arizona Diamondbacks.png'
+
 
 function Logos() {
   return (
-    <div>
-        Logos
-    </div>
+    <>
+    <section className='logo'>
+        <div>
+          <h3>MAJOR LEAGUE</h3>
+        </div>
+        <div className='logo__club'>
+          <img src={LogoNBA} alt=""/>
+          <h5 className='logo__club__title'>Phoenix Suns</h5>
+        </div>
+        <div className='logo__club'>
+          <img src={LogoNFL} alt=""/>
+          <h5 className='logo__club__title'>Arizona Cardinals</h5>
+        </div>
+        <div className='logo__club'>
+          <img src={LogoNHL} alt=""/>
+          <h5 className='logo__club__title'>Arizona Coyotes</h5>
+        </div>
+        <div className='logo__club'>
+          <img src={LogoMLB} alt=""/>
+          <h5 className='logo__club__title'>Arizona DBacks</h5>
+        </div>
+    </section>
+    <section className='logo'>
+        <div>
+          <h3>MINOR LEAGUE</h3>
+        </div>
+        <div className='logo__club'>
+          <img src={LogoNBA} alt=""/>
+          <h5 className='logo__club__title'>Phoenix Suns</h5>
+        </div>
+        <div className='logo__club'>
+          <img src={LogoNFL} alt=""/>
+          <h5 className='logo__club__title'>Arizona Cardinals</h5>
+        </div>
+        <div className='logo__club'>
+          <img src={LogoNHL} alt=""/>
+          <h5 className='logo__club__title'>Arizona Coyotes</h5>
+        </div>
+        <div className='logo__club'>
+          <img src={LogoMLB} alt=""/>
+          <h5 className='logo__club__title'>Arizona DBacks</h5>
+        </div>
+    </section>
+    <section className='logo'>
+        <div>
+          <h3>UNIVERSITY</h3>
+        </div>
+        <div className='logo__club'>
+          <img src={LogoNBA} alt=""/>
+          <h5 className='logo__club__title'>Phoenix Suns</h5>
+        </div>
+        <div className='logo__club'>
+          <img src={LogoNFL} alt=""/>
+          <h5 className='logo__club__title'>Arizona Cardinals</h5>
+        </div>
+        <div className='logo__club'>
+          <img src={LogoNHL} alt=""/>
+          <h5 className='logo__club__title'>Arizona Coyotes</h5>
+        </div>
+        <div className='logo__club'>
+          <img src={LogoMLB} alt=""/>
+          <h5 className='logo__club__title'>Arizona DBacks</h5>
+        </div>
+    </section>
+</>
   )
 }
 
