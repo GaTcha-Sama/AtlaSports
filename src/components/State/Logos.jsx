d56b7ec169f2099660fcf8c294a6d9f649d0b86b
Jeremy Louet <jeremy.louet29@hotmail.fr>
2024-06-11 20:46:52 +0200

    Alabama, Alaska & Arizona

--- src/components/State/Logos.jsx
+++ src/components/State/Logos.jsx
@@ -6,14 +6,15 @@ function Logos({ majorLeague, minorLeague, juniorLeague }) {
     <div className='logo'>
       {majorLeague.length > 0 && (
         <>
-          <h2 className='logo__level'>Major League Teams</h2>
+          <h2 className='logo__level'>Major League Teams :</h2>
           <div className='logo__teams'>
             {majorLeague.map(team => (
-              <div key={team.name} className='logo__club'>
-                <p className='logo__club__title'>{team.name}</p>
+              <div key={team.name} className='logo__club'>                
                 <div className='logo__club__content'>
                   <img src={team.logo} alt={team.name} className='logo__club__img'/>
                 </div>
+                <p className='logo__club__title'>{team.name}</p>
+                <img src={team.leagueLogo} alt={team.leagueLogo} className='logo__club__league'/>
               </div>
             ))}
           </div>
@@ -22,14 +23,15 @@ function Logos({ majorLeague, minorLeague, juniorLeague }) {
 
       {minorLeague.length > 0 && (
         <>
-          <h2 className='logo__level'>Minor League Teams</h2>
+          <h2 className='logo__level'>Minor League Teams :</h2>
           <div className='logo__teams'>
             {minorLeague.map(team => (
-              <div key={team.name} className='logo__club'>
-                <p className='logo__club__title'>{team.name}</p>
+              <div key={team.name} className='logo__club'>                
                 <div className='logo__club__content'>
                   <img src={team.logo} alt={team.name} className='logo__club__img'/>
                 </div>
+                <p className='logo__club__title'>{team.name}</p>
+                <img src={team.leagueLogo} alt={team.leagueLogo} className='logo__club__league'/>
               </div>
             ))}
           </div>
@@ -38,14 +40,15 @@ function Logos({ majorLeague, minorLeague, juniorLeague }) {
 
       {juniorLeague.length > 0 && (
         <>
-          <h2 className='logo__level'>Junior League Teams</h2>
+          <h2 className='logo__level'>Junior League Teams :</h2>
           <div className='logo__teams'>
             {juniorLeague.map(team => (
-              <div key={team.name} className='logo__club'>
-                <p className='logo__club__title'>{team.name}</p>
+              <div key={team.name} className='logo__club'>                
                 <div className='logo__club__content'>
                   <img src={team.logo} alt={team.name} className='logo__club__img'/>
                 </div>
+                <p className='logo__club__title'>{team.name}</p>
+                <img src={team.leagueLogo} alt={team.leagueLogo} className='logo__club__league'/>
               </div>
             ))}
           </div>
